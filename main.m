8d02a162c31d3925363b805afa96cb8375e96f85
DavidWilley13 <46052648+inoobs13@users.noreply.github.com>
2026-09-14 21:01:34 -0400

    First Version

---
+++ main.m
@@ -0,0 +1,18 @@
+%|-----------------------------------------------------------------------
+%|
+%|------MAKE SURE TO RUN init.m WITH YOUR FILES IN THE INPUT VALUES------
+%|
+%|------ALSO, MAKE SURE YOUR .csv FILES ARE IN THE PATH OF THE PROGRAM---
+%|------ONCE COMPLETED, RUN THIS SCRIPT TO PLOT THE FEATURES-------------
+%|
+
+close all;
+
+%| --- Plot Functions --- 
+
+plotFlightData(ftime, falt, "Flight", otime, oalt, "OpenRocket Sim", "Time (s)", ...
+    "Altitude AGL (ft)", "Altitude Data", "Altitude") % Altitude
+plotFlightData(ftime, fvv, "Flight", otime, ovv, "OpenRocket Sim", "Time (s)", ...
+    "Vertical Velocity (ft/s)", "Vertical Velocity", "Vert Vel.") % Vertical Velocity
+plotFlightData(ftime, fvt, "Flight", otime, ovt, "OpenRocket Sim", "Time (s)", ...
+    "Total Velocity (ft/s)", "Total Velocity", "Total Vel.") % Total Velocity
